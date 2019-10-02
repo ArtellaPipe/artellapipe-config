7af36f53f62a738c182f73ef0edef389bc51ece4
Tomi Poveda <tpovedatd@gmail.com>
2019-10-02 18:59:21 +0200

    artellapipe-config 0.0.2

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,14 +5,7 @@
 Module that contains tests for artellapipe-config
 """
 
-import os
-import sys
 import pytest
-
-source_path = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'source')
-if source_path not in sys.path:
-    sys.path.append(source_path)
 
 from artellapipe.config import __version__
 
